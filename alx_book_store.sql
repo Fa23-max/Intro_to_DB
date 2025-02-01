dc0c94590fd7c00bd388d03603385038afb75f3e
Joseph <musilijose618@gmail.com>
2025-02-01 12:00:25 +0300

    Intro to DB

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,38 +1,38 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
-CREATE TABLE authors (
+CREATE TABLE Authors (
   author_id int(11) NOT NULL,
   author_name varchar(215) NOT NULL,
   PRIMARY KEY (author_id)
 );
-CREATE TABLE books (
+CREATE TABLE Books (
   book_id int(11) NOT NULL,
   title varchar(130) NOT NULL,
   author_id int(11) NOT NULL,
   price double NOT NULL,
   publication_date date NOT NULL,
   PRIMARY KEY (book_id),
-  FOREIGN KEY (author_id) REFERENCES authors(author_id)
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
-CREATE TABLE customers (
+CREATE TABLE Customers (
   customer_id int(11) NOT NULL,
   customer_name varchar(215) NOT NULL,
   email varchar(215) NOT NULL,
   address text NOT NULL,
   PRIMARY KEY (customer_id)
 );
-CREATE TABLE orders (
+CREATE TABLE Orders (
   order_id int(11) NOT NULL,
   customer_id int(11) NOT NULL,
   order_date date NOT NULL,
   PRIMARY KEY (order_id),
-  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-CREATE TABLE order_details (
+CREATE TABLE Order_details (
   orderdetailid int(11) NOT NULL,
   order_id int(11) NOT NULL,
   book_id int(11) NOT NULL,
   quantity double NOT NULL,
   PRIMARY KEY (orderdetailid),
-  FOREIGN KEY (order_id) REFERENCES orders(order_id),
-  FOREIGN KEY (book_id) REFERENCES books(book_id)
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
